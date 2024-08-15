967db8d087b4a4024d01a14e749052d24ba8a022
Faeueu <rafaealbuquerquecz5@gmail.com>
2024-08-15 00:11:41 -0300

    users validation

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,21 +2,25 @@ package controller
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/Faeueu/GoLang-CRUD.git/src/configuration/rest_err"
+	//"github.com/Faeueu/GoLang-CRUD.git/src/configuration/rest_err"
+	"github.com/Faeueu/GoLang-CRUD.git/src/configuration/validation"
 	"github.com/Faeueu/GoLang-CRUD.git/src/controller/model/request"
+	//"github.com/Faeueu/GoLang-CRUD.git/src/controller/model/response"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
+	log.Println("Init CreateUsers controller")
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("Campo incorreto, error=%s", err.Error()))
+		log.Printf("Error trying to marshal object, error=%s", err.Error())
+		errRest := validation.ValidateUserError(err)
 		
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
-	fmt.Println(userRequest)
+	fmt.Print(userRequest)
 }
